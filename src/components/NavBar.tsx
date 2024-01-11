cc59ac34e6890574c597e52eeb0199906e8c3a20
tod1az <t.diaz.soto@gmail.com>
2024-01-11 10:53:51 -0300

    fix navbar bug

--- src/components/NavBar.tsx
+++ src/components/NavBar.tsx
@@ -8,7 +8,7 @@ export default function NavBar() {
   return (
     <nav
       className={`top-0 bg-blanco flex justify-center items-center
-         fixed  h-[4.5rem] text-2xl shadow-xl  w-screen
+         fixed  h-[4.5rem] text-2xl shadow-xl  w-screen z-[999]
          
          `}>
       <Drawer />
