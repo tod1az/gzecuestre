c66ecaa6d27901512b6a8ea0b7a9f2fa91bb6a83
tod1az <t.diaz.soto@gmail.com>
2024-01-19 14:53:57 -0300

    change slider width

--- src/components/filters/FilterSlider.tsx
+++ src/components/filters/FilterSlider.tsx
@@ -7,7 +7,7 @@ export function FilterSlider({ className, ...props }: SliderProps) {
   return (
     <Slider
       defaultValue={[0]}
-      className={cn("w-[90%]", className)}
+      className={cn("w-full", className)}
       {...props}
     />
   )
