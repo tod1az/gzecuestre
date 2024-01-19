cc1648caa2a528ae55ddd2428cf7c658c7e2a116
tod1az <t.diaz.soto@gmail.com>
2024-01-19 19:47:47 -0300

    add horse detail layout and dynamic metadata

---
+++ src/app/horses/[id]/layout.tsx
@@ -0,0 +1,23 @@
+import { getHorse } from "@/lib/querys"
+import { type Metadata } from "next"
+
+type Props = {
+  params: { id: string }
+}
+export async function generateMetadata(
+  { params }: Props,
+): Promise<Metadata> {
+  const id = params.id
+  const horse = await getHorse(id)
+  return {
+    title: `GZ ECUESTRE | ${horse?.name}`
+  }
+}
+
+export default function layout({ children }: { children: React.ReactNode }) {
+  return (
+    <>
+      {children}
+    </>
+  )
+}
