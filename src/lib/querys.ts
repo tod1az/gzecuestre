7c77f61e005aef878e0f826fcabb877df179fc23
tod1az <t.diaz.soto@gmail.com>
2024-01-23 19:10:40 -0300

    add querys with data base

--- src/lib/querys.ts
+++ src/lib/querys.ts
@@ -1,27 +1,36 @@
-import { horses, breeds } from './data'
 import { setFilters } from './filters'
+import { horseQueryModel } from './querymodels'
 import { HomeSearchParams } from './types'
+import prisma from '@/db/db'
 
 const perPage = 20
 export const getHorse = async (id: string) => {
-  return await horses.find((horse) => horse.id === id)
+  return await prisma.caballo.findFirst({
+    where: {
+      id
+    },
+    select: horseQueryModel
+  })
 }
 
 export const getHorses = async (searchParams: HomeSearchParams) => {
   const page = (Number(searchParams.page) ?? 1) - 1
   const order = searchParams.orden ?? 'asc'
   const filters = setFilters(searchParams)
-  //return await prisma.horses.findMany({
-  //skip:page*perPage
-  //take:perPage
-  //where: filters,
-  //orderBy:{
-  //price:order
-  //}
-  //})
-  return await horses
+  return await prisma.caballo.findMany({
+    where: filters,
+    select: horseQueryModel
+  })
 }
 
 export const getBreeds = async () => {
-  return await breeds
+  return await prisma.raza.findMany()
+}
+
+export const getDiscipline = async () => {
+  return await prisma.disciplina.findMany()
+}
+
+export const getProvinces = async () => {
+  return prisma.provincia.findMany()
 }
