2f9017898793be34bf4a705e08706375cdf678de
tod1az <t.diaz.soto@gmail.com>
2024-01-23 19:09:18 -0300

    adapt card to the new data model

--- src/components/cards/Card.tsx
+++ src/components/cards/Card.tsx
@@ -9,6 +9,7 @@ type CardProps = {
 }
 
 export default function Card({ horse }: CardProps) {
+  console.log(horse)
   return (
     <Link href={`/horses/${horse.id}`}>
       <article className="group hover:bg-orange-100/50 hover:text-negro transition-colors flex flex-col  text-black/80 rounded-xl gap-1 text-center h-[18rem] w-[11rem]  md:h-[20rem] md:w-[14rem] bg-blanco shadow-xl overflow-hidden">
@@ -23,12 +24,12 @@ export default function Card({ horse }: CardProps) {
           />
         </div>
         <div className="text-left px-7 mb-3 md:mb-4">
-          <h2 className="mt-4 text-sm font-bold">{horse.name}</h2>
-          <h3 className="font-bold" >{`$${horse.price}`}</h3>
+          <h2 className="mt-4 text-sm font-bold">{horse.nombre}</h2>
+          <h3 className="font-bold" >{`$${horse.precio}`}</h3>
         </div>
         <section className="px-7 flex flex-wrap text-center text-nowrap gap-1  md:gap-2 justify-center">
-          <CardBadge className="group-hover:bg-white group-hover:text-black">{horse.breed}</CardBadge>
-          <CardBadge className="group-hover:bg-white group-hover:text-black">{`${getAge(horse.birthdate)} años`}</CardBadge>
+          <CardBadge className="group-hover:bg-white group-hover:text-black">{horse.raza?.nombre}</CardBadge>
+          <CardBadge className="group-hover:bg-white group-hover:text-black">{`${getAge(horse.fecha_de_nacimiento)} años`}</CardBadge>
           <CardBadge className="group-hover:bg-white group-hover:text-black">{horse.sex}</CardBadge>
         </section>
       </article>
