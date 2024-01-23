936b7feaea72528e12c3eaa24e95ef47158ab088
tod1az <t.diaz.soto@gmail.com>
2024-01-23 14:45:02 -0300

    fix hero layout

--- src/components/landing/HeroSection.tsx
+++ src/components/landing/HeroSection.tsx
@@ -12,18 +12,18 @@ export default function Hero() {
     <section
       ref={ref}
       id="intro"
-      className={`flex flex-col scroll-m-[1000rem] xl:scroll-m-[1000rem]  md:scroll-m-[-1rem]  justify-center md:mt-[4.5rem]
-       gap-5 md:gap-[3rem] xl:gap-[10rem] items-center h-[91.5vh] md:h-[93vh] xl:h-[92.05vh] mb-32 `}>
-      <section className="flex flex-col xl:flex-row  items-center text-left xl:items-center gap-6 xl:gap-[7rem]">
-        <article className="flex flex-col px-3  gap-2  xl:gap-5">
-          <p className="text-2xl LandingSubTitle" >Bienvenidos</p>
-          <h1 className="LandingTitle font-bold text-5xl md:text-5xl">
+      className={`flex flex-col justify-center 
+       gap-5 md:gap-[1rem] xl:gap-[10rem] items-center h-[84.5vh] md:scroll-m-[1rem] md:h-[82vh] xl:h-[92.05vh] mb-32 `}>
+      <section className="flex flex-col xl:flex-row w-[95%]  items-center text-left xl:items-center gap-6 xl:gap-[7rem]">
+        <article className="flex flex-col   gap-2  xl:gap-5">
+          <p className="text-xl md:text-2xl LandingSubTitle" >Bienvenidos a</p>
+          <h1 className="LandingTitle font-bold text-4xl md:text-5xl">
             Gz Ecuestre
           </h1>
-          <p className="text-2xl md:text-3xl xl:text-2xl LandingSubTitle">
+          <p className="text-xl md:text-3xl xl:text-2xl LandingSubTitle">
             El mejor lugar para la compra y venta de <strong>caballos</strong>
           </p>
-          <section className="flex justify-around md:justify-center md:gap-[7rem] pt-6 xl:pl-10">
+          <section className="flex justify-between md:justify-center md:gap-[7rem] pt-6 xl:pl-10">
             <Button
               className={` flex gap-2 items-center bg-white/50
              text-negro text-lg md:text-xl hover:bg-negro hover:text-white
@@ -47,7 +47,7 @@ export default function Hero() {
           </section>
         </article>
         <Image
-          className="rounded-lg w-[22.5rem] md:w-auto  md:h-[368px]"
+          className="rounded-lg w-full md:w-auto  md:h-[368px]"
           src={'/caballos.webp'}
           width={480}
           height={288}
