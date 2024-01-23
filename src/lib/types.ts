a137774b58b8ae7a639493551ca8a35d4d221fdc
tod1az <t.diaz.soto@gmail.com>
2024-01-23 19:10:54 -0300

    add horse type

--- src/lib/types.ts
+++ src/lib/types.ts
@@ -1,8 +1,37 @@
 import { Links } from './data'
-import { horses } from './data'
-export type Horse = (typeof horses)[number]
 
 export type SectionName = (typeof Links)[number]['name']
+
+export type Horse = {
+  alzada: number
+  created_at: Date
+  descripcion: string
+  destacado: boolean | null
+  disciplina: {
+    nombre: string
+    id: string
+  } | null
+  fecha_de_nacimiento: Date
+  nombre: string
+  nombre_contacto: string
+  email: string
+  imagenes: string[]
+  id: string
+  numero: string
+  precio: number
+  precio_visible: boolean | null
+  provincia: {
+    nombre: string
+    id: string
+  } | null
+  raza: {
+    nombre: string
+    id: string
+  } | null
+  salto: number
+  sex: string
+  videos: string[] | null
+}
 
 export type HomeSearchParams = {
   page?: string
