abf0778b7ad9cb30d1e17e515d134d1e0928d652
tod1az <t.diaz.soto@gmail.com>
2024-01-27 17:16:24 -0300

    add home button

---
+++ src/components/HomeNavigation.tsx
@@ -0,0 +1,33 @@
+'use client'
+import { usePathname, useRouter } from "next/navigation"
+import { IoMdArrowBack } from "react-icons/io"
+import { FaHome } from "react-icons/fa";
+
+export default function HomeNavigation() {
+  const path = usePathname()
+  const { back, push } = useRouter()
+  if (path.startsWith('/horses')) {
+    return (
+      <button
+        aria-label="Back Button"
+        onClick={() => back()}
+        className="ml-3 md:ml-4 text-2xl md:text-xl xl:text-2xl flex gap-0 md:gap-2 text-gray-600 transition-colors items-center hover:underline hover:text-negro" >
+        <IoMdArrowBack />
+        <p className="hidden md:block">
+          Catálogo
+        </p>
+      </button >
+    )
+  }
+  return (
+    <button
+      aria-label="Back Button"
+      onClick={() => push('/')}
+      className="group ml-3  items-start md:ml-4 text-3xl xl:text-3xl flex gap-0 md:gap-2 transition-colors text-gray-600 hover:text-negro" >
+      <FaHome />
+      <p className="text-xl pt-1 hidden md:block">
+        Inicio
+      </p>
+    </button >
+  )
+}
--- src/components/NavBar.tsx
+++ src/components/NavBar.tsx
@@ -2,22 +2,23 @@ import Link from "next/link";
 import Logo from "./logos/Logo";
 import FiltersSheet from "./filters/FiltersSheet";
 import SheetContent from "./filters/SheetContent";
+import HomeNavigation from "./HomeNavigation";
 
 export default function NavBar() {
   return (
     <nav
-      className={`top-0 bg-blanco flex  justify-start  gap-[3rem]  md:justify-between items-center
+      className={`top-0 bg-blanco flex  justify-between  gap-[3rem]  m items-center
          fixed  h-[4.5rem] text-2xl shadow-xl  w-screen z-[10]`}>
-      <FiltersSheet>
-        <SheetContent />
-      </FiltersSheet>
+      <HomeNavigation />
       <Link
         href={'/'}>
         <div className="w-[10rem] h-auto md:w-[16rem]">
           <Logo />
         </div>
       </Link>
-      <div className="hidden md:block" />
+      <FiltersSheet>
+        <SheetContent />
+      </FiltersSheet>
     </nav>
   );
 }
