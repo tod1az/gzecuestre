de8c9fac3a73c8c42c66d9dc9d0d5dd35178a6f6
tod1az <t.diaz.soto@gmail.com>
2024-01-27 10:26:31 -0300

    change 404 sizing

--- src/components/cards/NotFound.tsx
+++ src/components/cards/NotFound.tsx
@@ -2,7 +2,7 @@ import Image from "next/image"
 export default function NotFound() {
   return (
     <Image
-      className="w-[100%] md:w-[70%] xl:w-[50%] h-auto rounded-xl  pb-[10rem] "
+      className="w-[100%] md:w-[70%] xl:w-[30%] h-auto rounded-xl  pb-[10rem] "
       src={'/404.webp'}
       alt="Sin Resultados"
       width={500}
