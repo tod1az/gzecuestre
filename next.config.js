c3666e1aae4d3f0f30f3ed0b74502898f81b0776
tod1az <t.diaz.soto@gmail.com>
2024-01-27 13:44:19 -0300

    add domain to the next config

--- next.config.js
+++ next.config.js
@@ -1,4 +1,15 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {}
+const nextConfig = {
+  images: {
+    remotePatterns: [
+      {
+        protocol: 'https',
+        hostname: 'vlbchjbofhbzpvjmbwjc.supabase.co',
+        port: '',
+        pathname: '/storage/v1/object/public/imagenes/**'
+      }
+    ]
+  }
+}
 
 module.exports = nextConfig
