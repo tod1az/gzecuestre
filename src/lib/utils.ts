12e7a344ffdc714ca2fee2d05ab12bd56ed52b90
tod1az <t.diaz.soto@gmail.com>
2024-01-30 07:31:12 -0300

    add time counter helper function

--- src/lib/utils.ts
+++ src/lib/utils.ts
@@ -15,3 +15,33 @@ export const getAge = (birthdate: Date) => {
   const milisecondsInAyear = 365.25 * 24 * 60 * 60 * 1000
   return Math.round((Date.now() - birthdate.getTime()) / milisecondsInAyear)
 }
+
+export function calcularTiempoTranscurrido(fechaPublicacion: Date) {
+  // Obten la fecha actual
+  const fechaActual = Date.now()
+  // Calcula la diferencia en milisegundos
+  const diferenciaMilisegundos = fechaActual - fechaPublicacion.getTime()
+
+  // Calcula los componentes de tiempo
+  const segundos = Math.floor(diferenciaMilisegundos / 1000)
+  const minutos = Math.floor(segundos / 60)
+  const horas = Math.floor(minutos / 60)
+  const dias = Math.floor(horas / 24)
+  const meses = Math.floor(dias / 30)
+  const años = Math.floor(meses / 12)
+
+  // Determina la cadena de tiempo transcurrido
+  if (años > 0) {
+    return años + (años === 1 ? ' año' : ' años')
+  } else if (meses > 0) {
+    return meses + (meses === 1 ? ' mes' : ' meses')
+  } else if (dias > 0) {
+    return dias + (dias === 1 ? ' día' : ' días')
+  } else if (horas > 0) {
+    return horas + (horas === 1 ? ' hora' : ' horas')
+  } else if (minutos > 0) {
+    return minutos + (minutos === 1 ? ' minuto' : ' minutos')
+  } else {
+    return segundos + (segundos === 1 ? ' segundo' : ' segundos')
+  }
+}
