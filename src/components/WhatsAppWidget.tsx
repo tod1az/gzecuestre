481e8efc086578cffc5b828c8e6f54c53cc5bcc8
tod1az <t.diaz.soto@gmail.com>
2024-02-02 12:07:27 -0300

    delete hover card

--- src/components/WhatsAppWidget.tsx
+++ src/components/WhatsAppWidget.tsx
@@ -1,6 +1,5 @@
 import Link from "next/link";
 import WhatsAppLogo from "./logos/WhatsAppLogo";
-import WhatsappHoverCard from "./WhatsappHoverCard";
 
 type Props = {
   href: string
@@ -14,14 +13,12 @@ export default function WhatsAppWidget({ href }: Props) {
       target="_blank"
       className={`fixed flex items-center hover:scale-125 transition-transform
     bottom-5 right-4 xl:right-16`}  >
-      <WhatsappHoverCard>
-        <div className="hidden md:block ">
-          <WhatsAppLogo size="70" />
-        </div>
-        <div className="block md:hidden">
-          <WhatsAppLogo size="50" />
-        </div>
-      </WhatsappHoverCard>
+      <div className="hidden md:block ">
+        <WhatsAppLogo size="70" />
+      </div>
+      <div className="block md:hidden">
+        <WhatsAppLogo size="50" />
+      </div>
     </Link>
   )
 }
