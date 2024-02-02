aab0d2f190c168224598aaa40c762a4007b09b9b
tod1az <t.diaz.soto@gmail.com>
2024-02-02 13:00:17 -0300

    fix sponsors section positioning

--- src/components/landing/SponsorsSection.tsx
+++ src/components/landing/SponsorsSection.tsx
@@ -12,7 +12,7 @@ export default function Sponsors({ children }: SponsorsProps) {
     <section
       ref={ref}
       id="sponsors"
-      className="scroll-m-[4rem] flex flex-col items-center gap-[6rem]  xl:gap-[5rem] h-[91.5vh] md:h-[93vh] xl:h-[92.05vh] w-full">
+      className="scroll-m-[6rem] md:scroll-m-[4rem] flex flex-col items-center gap-[6rem]  xl:gap-[5rem] h-[91.5vh] md:h-[93vh] xl:h-[92.05vh] w-full">
       <LandingHeader>
         Auspiciantes
       </LandingHeader >
