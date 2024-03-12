0fe14fec680c4137354ba2236b7a13bf72f42a79
tod1az <t.diaz.soto@gmail.com>
2024-03-12 17:16:25 -0300

    fix pagination bug

--- src/components/ui/Pagination.tsx
+++ src/components/ui/Pagination.tsx
@@ -1,23 +1,22 @@
-'use client'
-import ResponsivePagination from 'react-responsive-pagination'
-import 'react-responsive-pagination/themes/classic.css'
-import { useUrlSearchParams } from '@/lib/hooks'
+"use client";
+import ResponsivePagination from "react-responsive-pagination";
+import "react-responsive-pagination/themes/classic.css";
+import { useUrlSearchParams } from "@/lib/hooks";
 
 type PaginationProps = {
-  totalItems: number
-}
+  totalItems: number;
+};
 
 export default function Pagination({ totalItems }: PaginationProps) {
-  const { setPage, getCurrentPage } = useUrlSearchParams()
-  const perPage = 20
-  const totalPages = Math.trunc(totalItems / perPage)
-  if (totalPages === 1) return null
-  const initialPage = getCurrentPage(totalPages)
+  const { setPage, getCurrentPage } = useUrlSearchParams();
+  const perPage = 20;
+  const totalPages = Math.round(totalItems / perPage);
+  if (totalItems < perPage) return null;
+  const initialPage = getCurrentPage(totalPages);
 
   const handlePageChange = (page: number) => {
-    setPage(page)
-  }
-
+    setPage(page);
+  };
 
   return (
     <ResponsivePagination
@@ -25,5 +24,5 @@ export default function Pagination({ totalItems }: PaginationProps) {
       current={initialPage}
       onPageChange={(page: number) => handlePageChange(page)}
     />
-  )
+  );
 }
