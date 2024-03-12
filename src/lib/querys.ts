9d32e74c3860b40a2cb383b3115b69349c170599
tod1az <t.diaz.soto@gmail.com>
2024-03-12 17:07:49 -0300

    delete order

--- src/lib/querys.ts
+++ src/lib/querys.ts
@@ -1,109 +1,101 @@
-import { Prisma } from '@prisma/client'
-import { setFilters } from './filters'
-import { horseQueryModel } from './querymodels'
-import { HomeSearchParams } from './types'
-import prisma from '@/db/db'
+import { Prisma } from "@prisma/client";
+import { setFilters } from "./filters";
+import { horseQueryModel } from "./querymodels";
+import { HomeSearchParams } from "./types";
+import prisma from "@/db/db";
 
 export const getHorse = (id: string) => {
   return prisma.caballo.findFirst({
     where: {
-      id
+      id,
     },
-    select: horseQueryModel
-  })
-}
+    select: horseQueryModel,
+  });
+};
 
 export const getHorses = (searchParams: HomeSearchParams) => {
-  const perPage = 20
-  const page = Number(searchParams.page ?? 1)
-  const order = searchParams.orden ?? 'desc'
-  const filters = setFilters(searchParams)
+  const perPage = 20;
+  const page = Number(searchParams.page ?? 1);
+  const order = searchParams.orden ?? "desc";
+  const filters = setFilters(searchParams);
   return prisma.caballo.findMany({
-    orderBy: [
-      {
-        precio_visible: 'asc'
-      },
-      {
-        precio: order
-      }
-    ],
     where: filters as Prisma.CaballoWhereInput,
     select: horseQueryModel,
     skip: (page - 1) * perPage,
-    take: perPage
-  })
-}
+    take: perPage,
+  });
+};
 
 export const getFeaturedHorses = () => {
   return prisma.caballo.findMany({
     where: {
-      destacado: true
+      destacado: true,
     },
     select: {
       id: true,
-      imagenes: true
-    }
-  })
-}
+      imagenes: true,
+    },
+  });
+};
 
 export const getLastHorses = () => {
   return prisma.caballo.findMany({
     orderBy: [
       {
-        created_at: 'desc'
-      }
+        created_at: "desc",
+      },
     ],
     select: {
       id: true,
-      imagenes: true
+      imagenes: true,
     },
-    take: 5
-  })
-}
+    take: 5,
+  });
+};
 
 export const getBreeds = () => {
   return prisma.raza.findMany({
     orderBy: [
       {
-        nombre: 'asc'
-      }
-    ]
-  })
-}
+        nombre: "asc",
+      },
+    ],
+  });
+};
 
 export const getDisciplines = () => {
   return prisma.disciplina.findMany({
     orderBy: [
       {
-        nombre: 'asc'
-      }
-    ]
-  })
-}
+        nombre: "asc",
+      },
+    ],
+  });
+};
 
 export const getProvinces = () => {
   return prisma.provincia.findMany({
     orderBy: [
       {
-        nombre: 'asc'
-      }
-    ]
-  })
-}
+        nombre: "asc",
+      },
+    ],
+  });
+};
 
 export const getSponsors = () => {
   return prisma.auspiciante.findMany({
     orderBy: [
       {
-        nombre: 'asc'
-      }
-    ]
-  })
-}
+        nombre: "asc",
+      },
+    ],
+  });
+};
 
 export const getTotalHorses = (searchParams: HomeSearchParams) => {
-  const filters = setFilters(searchParams)
+  const filters = setFilters(searchParams);
   return prisma.caballo.count({
-    where: filters as Prisma.CaballoWhereInput
-  })
-}
+    where: filters as Prisma.CaballoWhereInput,
+  });
+};
