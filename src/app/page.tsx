6fc5abaa9e4b47eee0bc86acf4c1d87e9f08f420
tod1az <t.diaz.soto@gmail.com>
2024-03-24 10:38:17 -0300

    fix sponsors bug

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,31 +1,32 @@
-import Hero from "@/components/landing/HeroSection"
-import LandingNavBar from "@/components/landing/LandingNavBar"
-import FeaturedSection from "@/components/landing/FeaturedSection"
-import Sponsors from "@/components/landing/SponsorsSection"
-import ContactSection from "@/components/landing/ContactSection"
-import { ActiveSectionContextProvider } from "@/context/ActiveSectionContext"
-import About from "@/components/landing/About"
-import LandingCarousel from "@/components/landing/LandingCarousel"
-import { getLastHorses, getSponsors } from "@/lib/querys"
+import Hero from "@/components/landing/HeroSection";
+import LandingNavBar from "@/components/landing/LandingNavBar";
+import FeaturedSection from "@/components/landing/FeaturedSection";
+import Sponsors from "@/components/landing/SponsorsSection";
+import ContactSection from "@/components/landing/ContactSection";
+import { ActiveSectionContextProvider } from "@/context/ActiveSectionContext";
+import About from "@/components/landing/About";
+import LandingCarousel from "@/components/landing/LandingCarousel";
+import { getLastHorses, getSponsors } from "@/lib/querys";
 
 export default async function Home() {
-  const lastFive = await getLastHorses()
-  const sponsors = await getSponsors()
+  const lastFive = await getLastHorses();
+  const sponsors = await getSponsors();
   return (
-    <main
-      className="flex flex-col mt-[4.5rem] overflow-auto w-full  bg-gradient-to-br from-orange-300 to orange-100" >
+    <main className="flex flex-col mt-[4.5rem] overflow-auto w-full  bg-gradient-to-br from-orange-300 to orange-100">
       <ActiveSectionContextProvider>
         <LandingNavBar />
         <Hero />
         <FeaturedSection>
           <LandingCarousel items={lastFive} />
         </FeaturedSection>
-        <Sponsors>
-          <LandingCarousel sponsors={sponsors} />
-        </Sponsors>
+        {sponsors && (
+          <Sponsors>
+            <LandingCarousel sponsors={sponsors} />
+          </Sponsors>
+        )}
         <About />
         <ContactSection />
       </ActiveSectionContextProvider>
     </main>
-  )
+  );
 }
