a31b9897d1ac7cd4ba2fedfb59b36386ddd269e2
tod1az <t.diaz.soto@gmail.com>
2024-08-26 19:18:55 -0400

    fix: does not render the ads carousel, when there is no sponsors on the home page

--- src/components/ad/AdSv.tsx
+++ src/components/ad/AdSv.tsx
@@ -3,7 +3,6 @@ import Ad from "./Ad"
 
 export default async function AdSv({ location }: { location: number }) {
   const sponsors = await getSponsors()
-  return (
-    <Ad location={location} sponsors={sponsors} />
-  )
+  if (sponsors.length === 0) return null
+  return <Ad location={location} sponsors={sponsors} />
 }
--- src/components/cards/CardContainer.tsx
+++ src/components/cards/CardContainer.tsx
@@ -11,7 +11,9 @@ import { cn } from "@/lib/utils"
 type CardContainerProps = {
   searchParams: HomeSearchParams
 }
-export default async function CardContainer({ searchParams }: CardContainerProps) {
+export default async function CardContainer({
+  searchParams,
+}: CardContainerProps) {
   const horses = await getHorses(searchParams)
   const totalHorses = await getTotalHorses(searchParams)
   const adLocation = getAdLocation(horses)
@@ -19,37 +21,36 @@ export default async function CardContainer({ searchParams }: CardContainerProps
   return (
     <section className="flex flex-col items-center pb-[10rem] justify-center w-full  pt-10 p-6 bg-gradient-to-b from-orange-100 via-blanco to-orange-200 rounded-lg ">
       <section
-        className={cn("grid grid-cols-2 items-center justify-center rounded-lg xl:grid-cols-4 md:grid-cols-3 gap-[1rem] md:gap-[1rem]"
-          , { 'md:flex w-screen flex flex-wrap ': horses.length < 4 })}>
-        {
-          horses.map((horse, index) => {
-            if (index === adLocation[0]) {
-              return (
-                <Fragment key={'ad'}>
-                  <AdSv location={adLocation[0]} />
-                  <Card horse={horse} />
-                </Fragment>
-              )
-            }
-            if (index === adLocation[1]) {
-              return (
-                <Fragment key={'ad'}>
-                  <AdSv location={adLocation[1]} />
-                  <Card horse={horse} />
-                </Fragment>
-              )
-            }
+        className={cn(
+          "grid grid-cols-2 items-center justify-center rounded-lg xl:grid-cols-4 md:grid-cols-3 gap-[1rem] md:gap-[1rem]",
+          { "md:flex w-screen flex flex-wrap ": horses.length < 4 }
+        )}
+      >
+        {horses.map((horse, index) => {
+          if (index === adLocation[0]) {
             return (
-              <Fragment key={horse.id}>
+              <Fragment key={"ad"}>
+                <AdSv location={adLocation[0]} />
                 <Card horse={horse} />
               </Fragment>
             )
-          })
-        }
+          }
+          if (index === adLocation[1]) {
+            return (
+              <Fragment key={"ad"}>
+                <AdSv location={adLocation[1]} />
+                <Card horse={horse} />
+              </Fragment>
+            )
+          }
+          return (
+            <Fragment key={horse.id}>
+              <Card horse={horse} />
+            </Fragment>
+          )
+        })}
       </section>
-      {
-        horses.length <= 4 && <AdSv location={0} />
-      }
+      {horses.length <= 4 && <AdSv location={0} />}
       <div className="pt-[2rem] pb-2 ">
         <Pagination totalItems={totalHorses} />
       </div>
