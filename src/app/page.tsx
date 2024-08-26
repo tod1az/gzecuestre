acf56da84be3d1e6ef9c71ca351747bd6f7c5d40
tod1az <t.diaz.soto@gmail.com>
2024-08-26 18:58:04 -0400

    fix: does not render the sponsors carousel, when there is no sponsors

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,16 +1,16 @@
-import Hero from "@/components/landing/HeroSection";
-import LandingNavBar from "@/components/landing/LandingNavBar";
-import FeaturedSection from "@/components/landing/FeaturedSection";
-import Sponsors from "@/components/landing/SponsorsSection";
-import ContactSection from "@/components/landing/ContactSection";
-import { ActiveSectionContextProvider } from "@/context/ActiveSectionContext";
-import About from "@/components/landing/About";
-import LandingCarousel from "@/components/landing/LandingCarousel";
-import { getLastHorses, getSponsors } from "@/lib/querys";
+import Hero from "@/components/landing/HeroSection"
+import LandingNavBar from "@/components/landing/LandingNavBar"
+import FeaturedSection from "@/components/landing/FeaturedSection"
+import Sponsors from "@/components/landing/SponsorsSection"
+import ContactSection from "@/components/landing/ContactSection"
+import { ActiveSectionContextProvider } from "@/context/ActiveSectionContext"
+import About from "@/components/landing/About"
+import LandingCarousel from "@/components/landing/LandingCarousel"
+import { getLastHorses, getSponsors } from "@/lib/querys"
 
 export default async function Home() {
-  const lastFive = await getLastHorses();
-  const sponsors = await getSponsors();
+  const lastFive = await getLastHorses()
+  const sponsors = await getSponsors()
   return (
     <main className="flex flex-col mt-[4.5rem] overflow-auto w-full  bg-gradient-to-br from-orange-300 to orange-100">
       <ActiveSectionContextProvider>
@@ -19,7 +19,7 @@ export default async function Home() {
         <FeaturedSection>
           <LandingCarousel items={lastFive} />
         </FeaturedSection>
-        {sponsors && (
+        {sponsors.length > 0 && (
           <Sponsors>
             <LandingCarousel sponsors={sponsors} />
           </Sponsors>
@@ -28,5 +28,5 @@ export default async function Home() {
         <ContactSection />
       </ActiveSectionContextProvider>
     </main>
-  );
+  )
 }
