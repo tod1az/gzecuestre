056b8aadcbe48667dcf5770093cb818e2f04b6f1
tod1az <t.diaz.soto@gmail.com>
2024-08-26 19:26:36 -0400

    do not render sponsors on the navbar

--- src/lib/data.ts
+++ src/lib/data.ts
@@ -1,73 +1,73 @@
 export const featuredHorses = [
   {
-    imageRoute: '/caballo.jpg'
+    imageRoute: "/caballo.jpg",
   },
   {
-    imageRoute: '/caballo2.jpg'
+    imageRoute: "/caballo2.jpg",
   },
   {
-    imageRoute: '/caballo3.jpg'
+    imageRoute: "/caballo3.jpg",
   },
   {
-    imageRoute: '/caballo4.jpg'
-  }
+    imageRoute: "/caballo4.jpg",
+  },
 ]
 
 export const sponsors = [
   {
-    imageRoute: '/yourLogoHere.jpg'
+    imageRoute: "/yourLogoHere.jpg",
   },
   {
-    imageRoute: '/yourLogoHere2.png'
+    imageRoute: "/yourLogoHere2.png",
   },
   {
-    imageRoute: '/yourLogoHere3.png'
+    imageRoute: "/yourLogoHere3.png",
   },
   {
-    imageRoute: '/yourLogoHere4.jpg'
-  }
+    imageRoute: "/yourLogoHere4.jpg",
+  },
 ]
 export const Links = [
   {
-    hash: '#intro',
-    name: 'inicio'
+    hash: "#intro",
+    name: "inicio",
   },
   {
-    hash: '#new',
-    name: 'novedades'
+    hash: "#new",
+    name: "novedades",
+  },
+  // {
+  //   hash: '#sponsors',
+  //   name: 'auspiciantes'
+  // },
+  {
+    hash: "#about",
+    name: "nosotros",
   },
   {
-    hash: '#sponsors',
-    name: 'auspiciantes'
+    hash: "#contact",
+    name: "contacto",
   },
-  {
-    hash: '#about',
-    name: 'nosotros'
-  },
-  {
-    hash: '#contact',
-    name: 'contacto'
-  }
 ] as const
 
 export const Sexos = [
   {
-    id: '1',
-    nombre: 'macho'
+    id: "1",
+    nombre: "macho",
   },
   {
-    id: '2',
-    nombre: 'hembra'
-  }
+    id: "2",
+    nombre: "hembra",
+  },
 ]
 
 export const Orders = [
   {
-    id: '9878829347',
-    nombre: 'menor precio'
+    id: "9878829347",
+    nombre: "menor precio",
   },
   {
-    id: '9878829123347',
-    nombre: 'mayor precio'
-  }
+    id: "9878829123347",
+    nombre: "mayor precio",
+  },
 ]
